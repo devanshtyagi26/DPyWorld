1a3ec5d4bab5a84ce99ce0aafdb119faaab60da0
devanshtyagi26 <tyagidevansh3@gmail.com>
2022-12-13 19:44:53 +0530

    String Python Program

--- Remove all occurrences of a character from a string.py
+++ Remove all occurrences of a character from a string.py
@@ -1,11 +1,10 @@
-print("Remove the first occurrence of a character from a string\n")
+print("Remove all occurrences of a character from a string\n")
 str = input("Enter the string: ")
 char = input("Enter the character you want to remove: ")
 str1 = ""
-n = 0
 for i in range(len(str)):
-    if char == str[i] and n==0: 
-        n += 1
+    if char == str[i]: 
+        continue
     else:
         str1 += str[i]
 print()

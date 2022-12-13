6e7dcd51096fa5239cd71e9e7ebdd1971165cb22
Devansh Tyagi <118974690+devanshtyagi26@users.noreply.github.com>
2022-12-13 18:42:51 +0530

    Add files via upload

---
+++ Find the frequency of a character.py
@@ -0,0 +1,12 @@
+print("Find the frequency of a character in a given String\n")
+str = input("Enter the string: ")
+char = input("Enter the character: ")
+count = 0
+for i in range(len(str)):
+    if char == str[i]:
+        count += 1
+    else:
+        continue
+print()
+print()
+print(count)

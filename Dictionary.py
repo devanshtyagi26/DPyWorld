3743f9c60dea6becbdf0066995dc413bb142747d
devanshtyagi26 <tyagidevansh3@gmail.com>
2022-12-18 18:09:45 +0530

    Program on dictionary

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+print("Write a function that prints a dictionary where the keys are numbers between 1 and 5 and the values are cubes of the keys.\n")
+
+def cubdic():
+    dict = {}
+    for i in range(5):
+        dict[i] = i**3
+    print(dict)
+cubdic()
+
+print(" \nThanks for using this program... \nMade by Devansh Tyagi")
